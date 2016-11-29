36ec6929b2725f2436b5626964f1f2b1468dcae9
ahxxm <i@ahxxm.com>
2016-11-29 22:49:51 +0800

    JC will jump when C(arry)Flag is set

--- control/control.asm
+++ control/control.asm
@@ -11,22 +11,28 @@ main:
     mov         eax, 2
     mov         ebx, 3
 
+    cmp         eax, ebx
+    dump_regs   1
+
     ;; r = eax - ebx is computed
     ;; flags are set accordingly
     ;; - r == 0: ZF(zero flag) set
     ;; - r >  0: ZF unset, CF unset(no borrow)
     ;; - r <  0: ZF unset, CF set(borrow)
     ;; CF used as borrow flag for subtraction
-    cmp         eax, ebx
 
     ;; For signed int these 3 are important:
     ;; ZF, OF(overflow flag), SF(sign flag)
     ;; - r >  0: ZF unset, SF=OF
     ;; - r <  0: ZF unset, SF!=OF
 
-    ;; show debug info
-    dump_regs   1
+    ;; so after CMP, CF should be set
+    jc          exit0           ; jump when CF set
+    mov         ebx, 1          ; will never reach here
+    mov         eax, 1
+    int         0x80
 
+exit0:
     ;; exit
     mov         ebx, 0
     mov         eax, 1
